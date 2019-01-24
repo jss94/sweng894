e763afb1d5a65a3ed750ae520ef692b7280e80c6
zacharyeick <zacharyeick@gmail.com>
2019-01-23 20:54:03 -0500

    'EventOrganizer_Classes'

---
+++ source/Models/EventOrganizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace source.Models
+{
+    public class EventOrganizer
+    {
+        public int id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string displayName { get; set; }
+
+        public string emailAddress { get; set; }
+
+        public SecureString password { get; set; }
+
+        public string phoneNumber { get; set; }
+    }
+}
