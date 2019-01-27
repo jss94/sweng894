177be67d5272391b683198cf7228608cd9c841e1
Liz <finchliza@gmail.com>
2019-01-27 13:18:09 -0500

    wrote controllers for tests

--- source/Controllers/VendorsController.cs
+++ source/Controllers/VendorsController.cs
@@ -15,25 +15,63 @@ namespace source.Controllers
             _query = query;
         }
 
-        // GET api/users
+        /// <summary>
+        /// Gets all vendors
+        /// </summary>
+        /// <returns>List of Vendor</returns>
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return new OkObjectResult(await _query.GetAllAsync());
+            }
+            catch(Exception ex)
+            {
+                //TODO: we should log our errors in the db
+
+                return new BadRequestResult();
+            }
         }
 
-        // GET api/users
+        /// <summary>
+        /// Gets a vendor by the vendor id
+        /// </summary>
+        /// <param name="id">Vendor id</param>
+        /// <returns>Vendor</returns>
         [HttpGet]
         public async Task<IActionResult> GetById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return new OkObjectResult(await _query.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                //TODO: we should log our errors in the db
+
+                return new BadRequestResult();
+            }
         }
 
-        // GET api/users
+        /// <summary>
+        /// Gets a single vendor by user name
+        /// </summary>
+        /// <param name="userName">Vendor's unique user name </param>
+        /// <returns>Vendor</returns>
         [HttpGet]
-        public async Task<IActionResult> GetByName(string name)
+        public async Task<IActionResult> GetByName(string userName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return new OkObjectResult(await _query.GetByUserName(userName));
+            }
+            catch (Exception ex)
+            {
+                //TODO: we should log our errors in the db
+
+                return new BadRequestResult();
+            }
         }
     }
 }
