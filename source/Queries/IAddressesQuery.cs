11f7740bdbc040d1c8841a5e6f5758faf5cf216f
Peset Sam Tan <pesettan@gmail.com>
2019-01-28 14:54:08 -0800

    Added Address Query

---
+++ source/Queries/IAddressesQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using source.Models;
+
+namespace source.Queries
+{
+    public interface IAddressesQuery
+    {
+        Task<bool> Deactivate(Address address);
+        Task<List<Address>> GetAllAsync();
+        Task<Address> GetById(int id);
+        Task<Address> Insert(Address address);
+        Task<Address> Update(Address address);
+    }
+}
