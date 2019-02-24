103f47ec0de953d423ad0105f0f473813bf56243
Liz <finchliza@gmail.com>
2019-02-23 22:34:28 -0500

    fixed vendor services array in vendor model; unbroke vendor-details test

--- source/ClientApp/src/app/vendor-details/vendor-details.component.spec.ts
+++ source/ClientApp/src/app/vendor-details/vendor-details.component.spec.ts
@@ -1,32 +1,83 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, OnInit } from '@angular/core';
+import { FakeUser } from '../shared/models/fake-user.model';
 import { AuthService } from '../shared/services/auth.service';
-import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
-import { VendorServicesService } from '../vendor-services/Services/vendor-services.service';
-import { VendorService } from '../vendors/Services/vendor.service';
+import { VendorService } from 'src/app/vendors/Services/vendor.service';
 import { VendorServices } from '../shared/models/vendor-services.model';
 import { Vendor } from '../shared/models/vendor.model';
 import { Address } from '../shared/models/address.model';
 import { VendorDetailsComponent } from './vendor-details.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { MatSelectModule, MatFormFieldModule, MatInputModule, MatSnackBar, MatExpansionModule } from '@angular/material';
+import { of } from 'rxjs/internal/observable/of';
+import { Observable } from 'rxjs';
 
 describe('VendorDetailsComponent', () => {
   let component: VendorDetailsComponent;
   let fixture: ComponentFixture<VendorDetailsComponent>;
-  let mockVendorServicesService: VendorServicesService;
-  let mockVendorService: VendorService;
-  let mockVendorServices: VendorServices;
-  
+  let mockAuthService: AuthService;
+  let mockVendorSvc: VendorService;
+  let mockRouter: Router;
+
+  const fakeAddress: Address = {
+    userName: 'vendor@example.com',
+    street: '123 Main St.',
+    city: 'State College',
+    state: 'PA',
+    zip: 16801
+  };
+
+  const fakeService: VendorServices = {
+    id: 1,
+    vendorId: 1,
+    serviceType: 'Venue',
+    serviceName: 'Fake venue',
+    serviceDescription: 'Fake venue description',
+    flatFee: true,
+    price: 200.00,
+    unitsAvailable: null
+  };
+
+  const fakeSvcsArray = new Array(fakeService, fakeService, fakeService);
+
+  const fakeVendor: Vendor = {
+    id: 1,
+    userName: 'vendor@example.com',
+    name: 'Fake vendor',
+    website: 'www.psu.edu',
+    phone: '212-555-1212',
+    address: fakeAddress,
+    services: fakeSvcsArray
+  };
+
+
+  class MockAuthService {
+    user$ = of(new FakeUser);
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VendorDetailsComponent ]
+      declarations: [ VendorDetailsComponent ],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: Router, useValue: { navigate: () => {} } },
+        { provide: VendorService, useClass: VendorService }
+      ],
+      imports: [
+        MatSelectModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatExpansionModule,
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
 
+    
   beforeEach(() => {
+    mockAuthService = TestBed.get(AuthService);
+    mockVendorSvc = TestBed.get(VendorService);
     fixture = TestBed.createComponent(VendorDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -35,4 +86,5 @@ describe('VendorDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
