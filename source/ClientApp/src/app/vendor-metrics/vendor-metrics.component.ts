867ba4d2bcdde1d9d92f9f4ac05f3a3b10f41b41
Joe Senky <jss94@psu.edu>
2019-03-15 22:01:12 -0400

    Updated axis label

--- source/ClientApp/src/app/vendor-metrics/vendor-metrics.component.ts
+++ source/ClientApp/src/app/vendor-metrics/vendor-metrics.component.ts
@@ -25,7 +25,7 @@ export class VendorMetricsComponent implements OnInit {
   showXAxisLabel = true;
   xAxisLabel = 'Month';
   showYAxisLabel = true;
-  yAxisLabel = 'Count';
+  yAxisLabel = 'Reservations';
 
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
