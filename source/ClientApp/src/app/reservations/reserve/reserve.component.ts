66c3aea8c7a62795a7f989236051fdf1a365758a
Liz <finchliza@gmail.com>
2019-03-16 00:00:54 -0400

    reservations work

--- source/ClientApp/src/app/reservations/reserve/reserve.component.ts
+++ source/ClientApp/src/app/reservations/reserve/reserve.component.ts
@@ -9,7 +9,10 @@ import { VendorServicesService } from 'src/app/vendor-services/Services/vendor-s
 import { EventService } from 'src/app/events/Services/event.service';
 import { OccEvent } from 'src/app/events/Models/occ-event.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { GuestsService } from 'src/app/guests/Services/guests.service';
+import { subscribeOn } from 'rxjs/operators';
+import { Reservation } from '../Models/reservation.model';
+import { ReservationsService } from '../Services/reservations.service';
 
 
 @Component({
@@ -23,10 +26,12 @@ export class ReserveComponent implements OnInit {
   userName: string
   userEvents: OccEvent[]
   eventModel: OccEvent
-  eventList: []
+  eventGuestNum: number
+  cost: number
 
   reservationForm = new FormGroup({
-    eventList: new FormControl('', [ Validators.required ]),
+    eventList: new FormControl('', [Validators.required]),
+    numberReserved: new FormControl('', [Validators.required]),
   });
 
   constructor(
@@ -36,18 +41,21 @@ export class ReserveComponent implements OnInit {
     private dialog: MatDialog,
     private emailService: EmailService,
     private vendorServicesService: VendorServicesService,
-    private eventService: EventService
-    ) {
+    private eventService: EventService,
+    private guestService: GuestsService,
+    private reservationService: ReservationsService,
+  ) {
   }
 
   ngOnInit() {
+    this.cost = 0.00;
     this.setUser();
     this.setVendorService();
     this.getUserEvents();
+    this.subscribeCost();
   }
 
-  setUser()
-  {
+  setUser() {
     if (this.auth.user) {
       this.userName = this.auth.user.userName;
     } else {
@@ -61,35 +69,118 @@ export class ReserveComponent implements OnInit {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const svcId = +params.get('id');
       this.vendorServicesService.getVendorServiceById(svcId).subscribe(response => {
-          this.vendorServiceModel = response;
-        });
-      }, error => {
-        console.log(error);
+        this.vendorServiceModel = response;
       });
+    }, error => {
+      console.log(error);
+    });
   }
 
   getUserEvents() {
     this.eventService.getEvents(this.userName).subscribe(response => {
       this.userEvents = response
-      if(this.userEvents.length == 1){
-        this.eventModel = this.userEvents[0];        
+      if (this.userEvents.length == 1) {
+        this.eventModel = this.userEvents[0];
       }
-      if(this.userEvents.length > 1){
+      if (this.userEvents.length > 1) {
         this.subscribeEventChoice();
       }
     });
-    
+
   }
 
-  subscribeEventChoice(){
+  getNumGuests(value) {
+    let message = ""
+    this.guestService.getGuests(value).subscribe(guestresponse => {
+      if (guestresponse != null) {
+        this.eventGuestNum = guestresponse.length
+        if(this.vendorServiceModel.flatFee != true && this.vendorServiceModel.unitsAvailable < this.eventGuestNum){
+          this.reservationForm.controls["numberReserved"].setValue(this.vendorServiceModel.unitsAvailable)  
+          message ='This vendor cannot accomodate all of your guests with this service. Units Requested has been set to the units available.';
+          this.snackbar.open(message, '', {
+            duration: 5000
+          });   
+          //TODO: SET MAX IN UNITS REQUESTED CONTROL TO MAX OF VENDOR OFFERINGS
+        }
+        else{
+        this.reservationForm.controls["numberReserved"].setValue(this.eventGuestNum)
+        }
+      }
+    }, (error) => {
+      if (error.status === 404) {
+        this.reservationForm.controls["numberReserved"].setValue(0)
+        error = {
+          error: {
+            description: null
+          }
+        };
+      }
+      message = error.error.description || 'You have no guest list. We recommend adding a guest list.';
+
+      this.snackbar.open(message, '', {
+        duration: 5000
+      });
+    });
+  }
+
+  subscribeEventChoice() {
     this.reservationForm.controls['eventList'].valueChanges.subscribe(
       (value) => {
         if (value) {
           this.eventService.getEvent(value).subscribe(response => {
             this.eventModel = response
-       });
-      }
+            this.getNumGuests(value)
+          });
+        }
+      });
+  }
+
+  subscribeCost() {
+    this.reservationForm.controls['numberReserved'].valueChanges.subscribe(
+      (value) => {
+        if (value) {
+          if (this.vendorServiceModel != null) {
+            if (this.vendorServiceModel.flatFee == true) {
+              this.cost = this.vendorServiceModel.price
+            }
+            else {
+              this.cost = this.vendorServiceModel.price * value
+            }
+          }
+        } else {
+          this.cost = 0.00
+        }
+      });
+  }
+
+  onCreate(): void {
+    const res: Reservation = {
+      userName: this.userName,
+      eventId: this.eventModel.guid,
+      vendorId: this.vendorServiceModel.vendorId,
+      vendorServiceId: this.vendorServiceModel.id,
+      status: "New",
+      numberReserved: this.reservationForm.controls['numberReserved'].value,
+      service: null,
+      event: null,
+      vendor: null,
+      id: null
+     };
+
+    this.reservationService.createReservation(res).subscribe(response => {
+      this.ngOnInit();
+      this.reservationForm.reset();
+      this.snackbar.open('Your reservation has been requested.', 'Created', {
+        duration: 1500
+      });
+    }, error => {
+      console.log(error);
+      this.snackbar.open('Failed to create reservation request.', 'Failed', {
+        duration: 3500
+      });
     });
   }
+
+
 
 }
