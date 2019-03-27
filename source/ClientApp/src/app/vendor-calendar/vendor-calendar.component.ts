ee208b77a657a0f25bb0b59b18cf240d7c7fcf03
Joe Senky <jss94@psu.edu>
2019-03-26 22:41:46 -0400

    Updated to pull events from ngoninit. Removed un-used code

--- source/ClientApp/src/app/vendor-calendar/vendor-calendar.component.ts
+++ source/ClientApp/src/app/vendor-calendar/vendor-calendar.component.ts
@@ -1,27 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VendorEventService } from './Services/vendor-event.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import {
-  ChangeDetectionStrategy,
-  ViewChild,
-  TemplateRef
-} from '@angular/core';
+  ChangeDetectionStrategy} from '@angular/core';
 import {
   startOfDay,
   endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
   isSameDay,
-  isSameMonth,
-  addHours
-} from 'date-fns';
+  isSameMonth} from 'date-fns';
 import { Subject } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   CalendarEvent,
   CalendarEventAction,
-  CalendarEventTimesChangedEvent,
   CalendarView
 } from 'angular-calendar';
 
@@ -48,109 +39,50 @@ const colors: any = {
 })
 export class VendorCalendarComponent implements OnInit {
 
-  @ViewChild('modalContent') modalContent: TemplateRef<any>;
-
   view: CalendarView = CalendarView.Month;
 
   CalendarView = CalendarView;
 
   viewDate: Date = new Date();
 
-  modalData: {
-    action: string;
-    event: CalendarEvent;
-  };
-
-  actions: CalendarEventAction[] = [
-    /*
-    {
-      label: '<i class="fa fa-fw fa-pencil"></i>',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      }
-    },
-    {
-      label: '<i class="fa fa-fw fa-times"></i>',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.events = this.events.filter(iEvent => iEvent !== event);
-        this.handleEvent('Deleted', event);
-      }
-    }
-    */
-  ];
+  actions: CalendarEventAction[] = [];
 
   refresh: Subject<any> = new Subject();
 
-  events: CalendarEvent[] = [
-   /* {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: new Date(),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    }
-    */
-  ];
+  events: CalendarEvent[] = [];
 
   activeDayIsOpen = true;
 
   constructor(
     private route: ActivatedRoute,
-    private vendorEventsService: VendorEventService,
-    private modal: NgbModal
-  ) {}
+    private vendorEventsService: VendorEventService  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const vendorId = +params.get('vendorId');
       this.vendorEventsService.getVendorEvents(vendorId).subscribe(vendorEvents => {
         vendorEvents.forEach(vendorEvent => {
-          const title = 'Providing a ' + vendorEvent.serviceType + ' service for an event at ' + vendorEvent.eventTime;
-          const calendarEvent: CalendarEvent = ({
-            start: startOfDay(new Date(vendorEvent.eventDate)),
-            end: endOfDay(new Date(vendorEvent.eventDate)),
-            title: title,
-            color: colors.yellow,
-            actions: this.actions,
-            resizable: {
-              beforeStart: false,
-              afterEnd: false
-            },
-            draggable: false
-          });
-          this.events.push(calendarEvent);
+          const title = 'Providing a ' + vendorEvent.serviceType + ' service of '
+            + vendorEvent.serviceName + ' for an event at ' + vendorEvent.eventTime;
+
+          this.events = [
+            ...this.events,
+            {
+              start: startOfDay(new Date(vendorEvent.eventDate)),
+              end: endOfDay(new Date(vendorEvent.eventDate)),
+              title: title,
+              color: colors.yellow,
+              actions: this.actions,
+              resizable: {
+                beforeStart: false,
+                afterEnd: false
+              },
+              draggable: false
+           }
+          ];
+
         });
+        this.refresh.next();
       }, error => {
         console.log(error);
       });
@@ -170,55 +102,11 @@ export class VendorCalendarComponent implements OnInit {
       }
     }
   }
-/*
-  eventTimesChanged({
-    event,
-    newStart,
-    newEnd
-  }: CalendarEventTimesChangedEvent): void {
-    this.events = this.events.map(iEvent => {
-      if (iEvent === event) {
-        return {
-          ...event,
-          start: newStart,
-          end: newEnd
-        };
-      }
-      return iEvent;
-    });
-    this.handleEvent('Dropped or resized', event);
-  }
-*/
-  handleEvent(action: string, event: CalendarEvent): void {
-   /*
-    this.modalData = { event, action };
-    this.modal.open(this.modalContent, { size: 'lg' });
-    */
+
+  handleEvent(event: CalendarEvent): void {
     console.log(event.title);
   }
 
-  /*
-  addEvent(): void {
-    this.events = [
-      ...this.events,
-      {
-        title: 'New event',
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
-        color: colors.red,
-        draggable: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true
-        }
-      }
-    ];
-  }
-
-  deleteEvent(eventToDelete: CalendarEvent) {
-    this.events = this.events.filter(event => event !== eventToDelete);
-  }
-*/
   setView(view: CalendarView) {
     this.view = view;
   }
